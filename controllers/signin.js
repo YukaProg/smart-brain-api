d047a2c25f213067df709b07a462532c62452be3
Yuka <yukkuprog@gmail.com>
2018-10-31 12:39:53 +0900

    first commit

---
+++ controllers/signin.js
@@ -0,0 +1,43 @@
+const handleSignin = (req, res, db, bcrypt) => {
+  const { email, name, password } = req.body;
+  if(!email || !password){
+    return res.status(400).json('incorrect form submission');
+  }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      console.log(isValid);
+      if(isValid){
+        return db.select('*').from('users')
+        .where('email', '=', email)
+        .then(user => {
+          res.json(user[0])
+        })
+        .catch(err => res.status(400).json('unable to get user'))
+      }else{
+      res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+  //asynchronous
+  // bcrypt.compare("bacon", '$2a$10$QN.NdYkykMf5/a9ZBPkpxeqOtZwdWEWwJgpnkHmgodLbmdDAlJaba', function(err, res) {
+  //   console.log('first guess', res);
+  // });
+  // bcrypt.compare("veggies", '$2a$10$QN.NdYkykMf5/a9ZBPkpxeqOtZwdWEWwJgpnkHmgodLbmdDAlJaba', function(err, res) {
+  //   console.log('second guess', res);
+  // }); 
+
+
+  // if(req.body.email === database.users[0].email &&
+  //   req.body.password === database.users[0].password){
+  //   res.json(database.users[0]); 
+  // }else{
+  //   res.status(400).json('error logging in');
+  // }
+
+}
+
+module.exports = {
+  handleSignin: handleSignin
+};
