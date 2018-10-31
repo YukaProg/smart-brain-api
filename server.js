d346f79b8869ef6e3bb988990d2526aabce745b2
Yuka <yukkuprog@gmail.com>
2018-10-31 14:40:06 +0900

    adding port

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ const db = knex({
 // });
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 // const database = {
 //   users: [
@@ -56,9 +56,7 @@ app.use(cors());
 //   ]
 // }
 
-app.get('/', (req, res)=>{
-  res.send(database.users);
-})
+app.get('/', (req, res) => {res.send(database.users) });
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 
@@ -69,8 +67,8 @@ app.get('/profile/:id', (req, res) => {porfile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, ()=>{
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log(`app is running on port 3000 ${process.env.PORT}`);
 })
 
 /*
